dca2ab83ab31bdf53fafb91af6794d1c3f4f3b92
reyhaonan <reyhaonan2003@gmail.com>
2024-10-06 20:49:56 +0700

    modified:   src/components/HeroSection.svelte

--- src/components/HeroSection.svelte
+++ src/components/HeroSection.svelte
@@ -33,26 +33,30 @@
 			</div>
 		</div>
 
-		<div class="flavorText absolute bottom-32 right-20 w-1/4">
-			<svg
-				width="100%"
-				viewBox="0 0 541 167"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-				class="absolute-center z-30"
-			>
-				<path d="M60.6053 0H541L510.975 167L0 157.439L60.6053 0Z" fill="#1B1B1B" />
-			</svg>
+		<div class="flavorText absolute bottom-1/6 right-20 w-1/4 z-40">
+			<div class="absolute-center w-full">
+				<svg
+					width="100%"
+					viewBox="0 0 541 167"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+					class="animate-smootherJelly"
+				>
+					<path d="M60.6053 0H541L510.975 167L0 157.439L60.6053 0Z" fill="#1B1B1B" />
+				</svg>
 
-			<svg
-				width="90%"
-				viewBox="0 0 487 131"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-				class="absolute-center z-30"
-			>
-				<path d="M55 0H487L460 131L0.5 123.5L55 0Z" fill="#010101" />
-			</svg>
+				<div class="absolute-center w-10/12">
+					<svg
+						width="90%"
+						viewBox="0 0 487 131"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+						class="animate-smootherJelly"
+					>
+						<path d="M55 0H487L460 131L0.5 123.5L55 0Z" fill="#010101" />
+					</svg>
+				</div>
+			</div>
 
 			<div class="text-16 2xl:text-24 text-center w-2/3 mx-auto z-40 text-white font-bold relative">
 				I'm a Fullstack developer from Indonesia.
