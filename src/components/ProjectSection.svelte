f21cc00bf45a6f11bc489d94fdeefaf46186daa5
reyhaonan <reyhaonan2003@gmail.com>
2024-10-07 22:24:00 +0700

    another jelly

--- src/components/ProjectSection.svelte
+++ src/components/ProjectSection.svelte
@@ -15,24 +15,32 @@
 			</div>
 
 			<div class="description absolute -right-[48vw] w-[56vw] -rotate-[8deg] -top-8">
-				<svg width="100%" viewBox="0 0 1098 429" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<svg
+					width="100%"
+					class="animate-smootherJelly"
+					viewBox="0 0 1098 429"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
 					<path
 						d="M64.0264 2.09672L862.666 0.0602088L849.234 42.7354L1097.63 42.0942L975.997 428.542L138.147 354.397L208.831 175.353L0.277237 161.175L64.0264 2.09672Z"
 						fill="#1B1B1B"
 					/>
 				</svg>
-				<svg
-					class="absolute-center"
-					width="92%"
-					viewBox="0 0 990 367"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						d="M51.5868 0.211525L749.536 13.5909L736.003 62.2521L989.545 68.132L895.644 366.471L144.214 300.072L215.676 132.223L0.934342 122.568L51.5868 0.211525Z"
-						fill="#010101"
-					/>
-				</svg>
+				<div class="absolute-center w-11/12">
+					<svg
+						class="animate-[smootherJelly_1.1s_linear_infinite_alternate-reverse]"
+						width="100%"
+						viewBox="0 0 990 367"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d="M51.5868 0.211525L749.536 13.5909L736.003 62.2521L989.545 68.132L895.644 366.471L144.214 300.072L215.676 132.223L0.934342 122.568L51.5868 0.211525Z"
+							fill="#010101"
+						/>
+					</svg>
+				</div>
 
 				<h3
 					class="absolute left-1/12 top-[14%] font-bold italic text-32 2xl:text-48 text-outline rotate-2"
